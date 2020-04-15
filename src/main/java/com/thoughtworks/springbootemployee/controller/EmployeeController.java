b49cd72243a33785eee41efc98dbbc9c0ad84504
chowca <56864611+chowca@users.noreply.github.com>
2020-04-15 22:56:00 +0800

    [Calvin Chow] Complete interfaces for Employee (Post)

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -12,14 +12,24 @@ import java.util.List;
 public class EmployeeController {
     private List<Employee> employees = new ArrayList<>();
 
-    @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    public List<Employee> getAllEmployees() {
+    public EmployeeController(){
         employees.add(new Employee(0, "Xiaoming", 20, "Male"));
         employees.add(new Employee(1, "Xiaohong", 19, "Female"));
         employees.add(new Employee(2, "Xiaozhi", 15, "Male"));
         employees.add(new Employee(3, "Xiaogang", 16, "Male"));
         employees.add(new Employee(4, "Xiaoxia", 15, "Female"));
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Employee> getAllEmployees() {
         return employees;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee createNewEmployee(@RequestBody Employee employee){
+        employees.add(employee);
+        return employee;
     }
 }
